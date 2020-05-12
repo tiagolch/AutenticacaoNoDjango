8ad7de0e55660551370af22a1f9af3713bed62fc
Tiago <tiagolch@hotmail.com>
2020-05-11 23:53:16 -0300

    Criado sistema de autenticação de usuarios acompanhando o curso Devmedia.

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from .views import *
+
+urlpatterns = [
+    url( r'^registrar_usuario/$', registrar_usuario, name='registrar_usuario' ),
+    url( r'^listar_usuario/$', listar_usuario, name='listar_usuario' ),
+    url( r'^logar/$', logar, name='logar' ),
+    url( r'^remover_usuario/(?P<pk>[0-9]+)', remover_usuario, name='remover_usuario' ),
+    url( r'^deslogar/$', deslogar, name='deslogar' ),
+
+]
